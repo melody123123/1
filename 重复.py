dcbb62ee7db7819853f4dfbd085d7f2135431171
melody123123 <109729530+melody123123@users.noreply.github.com>
2025-06-27 08:14:27 +0800

    Create 重复.py

---
+++ 重复.py
@@ -0,0 +1,25 @@
+nums = [1, 2, 3, 4]
+squared = []
+for num in nums:
+    squared.append(num ** 2)
+print(squared)
+nums = [1, 2, 3, 4]
+squared = []
+for num in nums:
+    squared.append(num ** 2)
+print(squared)
+nums = [1, 2, 3, 4]
+squared = []
+for num in nums:
+    squared.append(num ** 2)
+print(squared)
+nums = [1, 2, 3, 4]
+squared = []
+for num in nums:
+    squared.append(num ** 2)
+print(squared)
+nums = [1, 2, 3, 4]
+squared = []
+for num in nums:
+    squared.append(num ** 2)
+print(squared)
